fd6d8f8f37d6d24fcf9641c28763aac3b821a7d0
zcfhzx@vip.qq.com <605908wsw>
2017-11-30 22:04:26 +0800

    矫克清 2017.11.30 1.导入jar包 2.Mybatis和Spring框架搭建和整合成功 3.单元测试类准备完成

---
+++ src/test/ProjectTest.java
@@ -0,0 +1,39 @@
+package test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+//单元测试类
+public class ProjectTest {
+
+    @Before
+    public void before(){
+
+    }
+
+    @Test
+    public void testInsert(){
+
+    }
+
+    @Test
+    public void testDelete(){
+
+    }
+
+    @Test
+    public void testUpdate(){
+
+    }
+
+    @Test
+    public void testSelect(){
+
+    }
+
+    @After
+    public void after(){
+
+    }
+}
